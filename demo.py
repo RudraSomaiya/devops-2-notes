bfbc4030bcf5f2886b624bc31198e4d3e896b79a
RudraSomaiya <rudrasomaiya0@gmail.com>
2026-04-22 11:07:46 +0530

    Fixed errors for pylint

--- demo.py
+++ demo.py
@@ -1,10 +1,13 @@
-def myFunction():
+def my_Function():
+    """
+    This is a test
+    """
 
     a = 4
     b = 6
-    password = '123'
-    c = a +  + password
+    # password = '123'
+    c = a + b
     return c
 
 
-myFunction()
+my_Function()
